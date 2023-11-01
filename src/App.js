933ca0433afb4796f761a6210d0715231b44ebf6
Edward Rogers <edwardrogers@linux-682676212dcc.home>
2023-11-01 21:02:57 +0000

    removed unused code and added basic button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  function handleClick() {
+    console.log("button has been clicked");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? <div>There is data</div> : <div>There is no data</div>}
+      <button onClick={handleClick}>Fetch data</button>
     </div>
   );
 }
